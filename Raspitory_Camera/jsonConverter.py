f8fe90cf93eebac8058e4bfcf2c138be5af3f68a
Laurenz Schmielau <laurenz.schmielau@brentwood.ca>
2018-11-26 19:19:26 -0800

    bug fixes

--- Raspitory_Camera/jsonConverter.py
+++ Raspitory_Camera/jsonConverter.py
@@ -43,8 +43,8 @@ SAME_PERSON_THRESHOLD_FACTOR = 0.1
 def person_matches(person1, person2):
     
 #    determined by shoulder width + hip to shoulder height
-    size_factor_person1 = (person1.left_shoulder_x - person1.right_shoulder_x + person1.right_hip_y - person1.right_shoulder_y) * SAME_PERSON_THRESHOLD_FACTOR
-    size_factor_person2 = (person2.left_shoulder_x - person2.right_shoulder_x + person2.right_hip_y - person2.right_shoulder_y) * SAME_PERSON_THRESHOLD_FACTOR
+    size_person1 = (person1.left_shoulder_x - person1.right_shoulder_x + person1.right_hip_y - person1.right_shoulder_y)
+    size_person1_factor = size_person1 * SAME_PERSON_THRESHOLD_FACTOR
     x1 = abs(person1.right_shoulder_x - person2.right_shoulder_x)
     x2 = abs(person1.left_shoulder_x - person2.left_shoulder_x)
     x3 = abs(person1.right_hip_x - person2.right_hip_x)
@@ -54,41 +54,38 @@ def person_matches(person1, person2):
     y3 = abs(person1.right_hip_y - person2.right_hip_y)
     y4 = abs(person1.left_hip_y - person2.left_hip_y)
     
-    if x1 <
-    return
+    if x1 < size_person1_factor and x2 < size_person1_factor and x3 < size_person1_factor and x4 < size_person1_factor and y1 < size_person1_factor and y2 < size_person1_factor and y3 < size_person1_factor and y4 < size_person1_factor:
+        return True
+    else:
+        return False
 
 
 def reorganize_to_by_person(chests_by_frame):
-#     somehow ensure order here...
     by_person = []
     max_people = 0
-    
     frame = chests_by_frame[0]
-    for person, index in frame:
-        by_person[index].append(person)
-        if index > max_people:
-            max_people = index + 1
-
+    for person in frame:
+        person_list = []
+        person_list.append(person)
+        by_person.append(person_list)
+        max_people = max_people + 1
+    
     for x in range(len(chests_by_frame) - 1):
         frame = chests_by_frame[x+1]
         for person_in_frame in frame:
             person_assigned = 0
             for person in by_person:
-                if (person_in_frame matches person):
+                if person_matches(person_in_frame, person[-1]):
                     person.append(person_in_frame)
                     person_assigned = 1
                     break
             if person_assigned == 0:
-                by_person[max_people].append(person_in_frame)
-    
+                person_list = []
+                person_list.append(person_in_frame)
+                by_person.append(person_list)
+                max_people = max_people + 1
+
     return by_person
-
-#            check if they fit any existing person else new person (dont forget to increase count)
-#if count > max:
-#    max = count
-#        max_person = index
-
-    return by_person, max, max_person
 
 # define
 SEQUENCE_THRESHOLD = 10
@@ -143,17 +140,14 @@ def set_sequence_coordinates(start, end, person):
 def extract_sequences(people):
     start = 0
     end = 0
-    people = []
+    people_sequences = []
 
     for person in people:
         sequences = []
         start = 0
         end = 0
         for index in range(len(person) - 1):
-            if abs(person[index].right_shoulder - person[index + 1].right_shoulder) > SEQUENCE_THRESHOLD and
-                abs(person[index].left_shoulder - person[index + 1].left_shoulder) > SEQUENCE_THRESHOLD and
-                abs(person[index].right_hip - person[index + 1].right_hip) > SEQUENCE_THRESHOLD and
-                abs(person[index].left_hip - person[index + 1].left_hip) > SEQUENCE_THRESHOLD:
+            if abs(person[index].right_shoulder_x - person[index + 1].right_shoulder_x) > SEQUENCE_THRESHOLD and abs(person[index].left_shoulder_x - person[index + 1].left_shoulder_x) > SEQUENCE_THRESHOLD and abs(person[index].right_hip_x - person[index + 1].right_hip_x) > SEQUENCE_THRESHOLD and abs(person[index].left_hip_x - person[index + 1].left_hip_x) > SEQUENCE_THRESHOLD:
 
                 end = index + 1
             else:
@@ -161,8 +155,8 @@ def extract_sequences(people):
                 sequences.append(sequence)
                 start = index
                 end = index
-            sequences.append(Sequence(start, end))
-        people.append(sequences)
+        sequences.append(set_sequence_coordinates(start, end, person))
+        people_sequences.append(sequences)
 
     return people
 
@@ -170,7 +164,7 @@ def extract_sequences(people):
 
 
 
-
+PATH = '/Users/laurenzschmielau/Desktop/CPEN491/openpose/examples/example_results_json/'
 #    TODO
 
 def test():
@@ -183,12 +177,13 @@ def test():
     keypoint_json = json.loads(keypoint_file)
     
     chests_by_frame = []
-    max = 0
-#    do for all files TODO
-    chests_by_frame.append(extract_frame_chests(keypoint_json))
     
-    chests_by_person, max, max_person = reorganize_to_by_person(chests_by_frame)
-    chests_by_person = ensure_order(chests_by_person, max, max_person)
+    filelist = os.listdir(PATH)
+    for file in filelist:
+        keypoint_json = json.loads(open(PATH + file, 'r').read())
+        chests_by_frame.append(extract_frame_chests(keypoint_json))
+
+    chests_by_person = reorganize_to_by_person(chests_by_frame)
     sequences_by_person = extract_sequences(chests_by_person)
 
     print(sequences_by_person)
